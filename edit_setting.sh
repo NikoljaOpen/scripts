5a2f0d84ee57b05fbbcf52762b1b950db29e8099
NikoljaOpen <nikoljahromoj@gmail.com>
2025-05-19 18:26:47 +0000

    fixed mistakes

--- edit_setting.sh
+++ edit_setting.sh
@@ -3,8 +3,8 @@ sed -i 's/^#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
 echo 'LANG="ru_RU.UTF-8"' >> /etc/locale.conf
 locale-gen
 systemctl enable NetworkManager
-timedatectl set-timezone /usr/share/zoneinfo/Europe/Moscow
+timedatectl set-timezone Europe/Moscow
 timedatectl set-ntp true
 echo 'arch_desktop' >> /etc/hostname
 echo '127.0.1.1    arch_desktop.localedomain    arch_desktop' >> /etc/hosts
-
+refind-install
--- own_packages.sh
+++ own_packages.sh
@@ -1,9 +1,7 @@
-mkfs.vfat /dev/sda1
+mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
 mkfs.ext4 /dev/sdb1
 mount /dev/sda2 /mnt
-mkdir -p /mnt/boot/efi
-mount /dev/sda1 /mnt/boot/efi
 mount --mkdir /dev/sdb1 /mnt/data
-pacstrap /mntacman -S base base-devel linux linux-firmware linux-headers nano vim bash-completion refind gdisk noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji ttf-jetbrains-mono wayland xorg-xwayland pipewire
-genfstab /mnt >> /mnt/etc/fstab
+pacstrap /mnt base base-devel linux linux-firmware linux-headers networkmanager nano vim bash-completion refind gdisk noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji ttf-jetbrains-mono wayland xorg-xwayland pipewire
+genfstab -U /mnt >> /mnt/etc/fstab
